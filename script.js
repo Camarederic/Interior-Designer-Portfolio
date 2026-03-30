42f1d8b61e51715ffbaa95367c3f901fb4e051a5
Camarederic <camarederic@gmail.com>
2026-03-30 21:33:17 +0300

    added code and styles for the hamburger menu JS & CSS

--- script.js
+++ script.js
@@ -1,2 +1,13 @@
-'use strict'
+"use strict";
 
+const menuIcon = document.querySelector(".menu-icon");
+const xBtn = document.querySelector(".x-btn");
+const navigation = document.querySelector(".navigation");
+
+menuIcon.addEventListener("click", () => {
+  navigation.classList.add("navigate");
+});
+
+xBtn.addEventListener("click", () => {
+  navigation.classList.remove("navigate");
+});
